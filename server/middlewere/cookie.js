50b28920093b9ad6cb6dbe7c25268b062718bf1e
Maran077 <marvelmaran07@gmail.com>
2024-02-15 23:37:52 +0530

    check jwt token

--- server/middlewere/cookie.js
+++ server/middlewere/cookie.js
@@ -3,11 +3,17 @@ const UserModel = require("../mongodb/userSchema.js");
 const ErrorHandler = require("./errorHandler.js");
 
 const cookieVerify = async (req, res, next) => {
+  const errMsg = {
+    message: "please login first",
+    code: 404,
+  };
   const { token } = req.query;
-  if (!token) return next(new ErrorHandler("please login first", 404));
+  if (!token) return next(new ErrorHandler(errMsg.message, errMsg.code));
+  const parts = token.split(".");
+  if (parts.length !== 3) next(new ErrorHandler(errMsg.message, errMsg.code));
   const decoded = jwt.verify(token, process.env.JWT_KEY);
   const user = await UserModel.findOne({ userId: decoded.id });
-  if (!user) return next(new ErrorHandler("please login first", 404));
+  if (!user) return next(new ErrorHandler(errMsg.message, errMsg.code));
   req.user = user;
   next();
 };
